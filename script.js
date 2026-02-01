a9f5445f5e5463ee6525c46532749cd498a087e7
ayushcortex <ayushkumarsingh956@gmail.com>
2026-02-01 16:43:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,19 @@ const MODEL_URL = "/attendance-system/models";
 
 async function loadModels() {
   try {
-    statusText.innerText = "⏳ Loading models...";
-
+    statusText.innerText = "⏳ Loading tiny face detector...";
     await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+
+    statusText.innerText = "⏳ Loading face landmarks...";
     await faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL);
+
+    statusText.innerText = "⏳ Loading face recognition...";
     await faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL);
 
     statusText.innerText = "✅ Models loaded successfully";
-    console.log("Models loaded successfully");
+    console.log("All models loaded");
   } catch (err) {
-    console.error("MODEL LOAD ERROR:", err);
+    console.error("MODEL LOAD FAILED:", err);
     statusText.innerText = "❌ Model loading error";
   }
 }
